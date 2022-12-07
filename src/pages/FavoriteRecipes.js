991227ed09ea866e29e24ba85b834d286fa7d3a0
Eric Maike <ericmaikelopes@gmail.com>
2022-12-07 15:25:50 -0400

    Requisitos de 50-56 sem testes

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,72 +1,170 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import clipboardCopy from 'clipboard-copy';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import drinkIcon from '../images/drinkIcon.svg';
 
+const favoritas = [{
+  id: 52771,
+  type: 'meal',
+  nationality: 'Italian',
+  category: 'Vegetarian',
+  alcoholicOrNot: 'not',
+  name: 'Spicy Arrabiata Penne',
+  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+},
+
+{
+  id: 178319,
+  type: 'drink',
+  nationality: 'brasileiro',
+  category: 'Cocktail',
+  alcoholicOrNot: 'Alcoholic',
+  name: 'Aquamarine',
+  image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+},
+];
 class FavoriteRecipes extends Component {
+  // const favoritas = localStorage.getItem('favoriteRecipes');
+
+  state = {
+    filtereds: favoritas,
+    clickShare: false,
+  };
+
+  desfavoritar = (id) => {
+    // localStorage.removeItem('favoriteRecipes');
+    localStorage
+      .setItem('favoriteRecipes', [...favoritas, favoritas
+        .filter((recipe) => recipe.id !== id)]);
+  };
+
+  clickFilter = (type) => {
+    this.setState({ filtereds: favoritas.filter((recipe) => recipe.type !== type) });
+  };
+
   render() {
     const { history } = this.props;
-    // const favoritas = localStorage.getItem('favoriteRecipes');
-    const favoritas = [{
-      id: 0,
-      type: 'meal',
-      nationality: 'brasileiro',
-      category: '',
-      alcoholicOrNot: 'not',
-      name: 'churrasco',
-      image: 'https://st1.uvnimg.com/3d/28/5601ad8e4d32a7272e3b7a8d8110/churrasco-0522.jpg',
-    },
-
-    {
-      id: 1,
-      type: 'drink',
-      nationality: 'brasileiro',
-      category: '',
-      alcoholicOrNot: 'alcoholic',
-      name: 'caipirinha',
-      image: 'https://img.itdg.com.br/images/recipes/000/128/825/319875/319875_original.jpg',
-    },
-    ];
-
+    const { filtereds, clickShare } = this.state;
     return (
       <div>
         <Header history={ history } />
 
-        <button type="button" data-testid="filter-by-all-btn">
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => { this.clickFilter(); } }
+        >
           All
         </button>
 
-        <button type="button" data-testid="filter-by-meal-btn">
+        <button
+          type="button"
+          data-testid="filter-by-meal-btn"
+          onClick={ () => { this.clickFilter('drink'); } }
+        >
           <img src={ mealIcon } alt="ícone de comida" />
         </button>
 
-        <button type="button" data-testid="filter-by-drink-btn">
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => { this.clickFilter('meal'); } }
+        >
           <img src={ drinkIcon } alt="ícone de bebida" />
         </button>
 
-        {favoritas.map((recipe, index) => {
+        {clickShare && <p>Link copied!</p>}
+
+        {filtereds.map((recipe, index) => {
           const { id, type, nationality, category, alcoholicOrNot, name, image } = recipe;
+          if (type === 'meal') {
+            return (
+              <div key={ id }>
+                <Link to={ `/meals/${id}` }>
+                  <img
+                    src={ image }
+                    alt={ name }
+                    data-testid={ `${index}-horizontal-image` }
+                    style={ { width: '360px', weight: '640px' } }
+                  />
+                </Link>
+
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {`${nationality} - ${category}`}
+
+                </p>
+
+                <Link to={ `/meals/${id}` }>
+                  <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+                </Link>
+
+                <button
+                  type="button"
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  src={ shareIcon }
+                  onClick={ () => {
+                    clipboardCopy(`http://localhost:3000/meals/${id}`);
+                    this.setState({ clickShare: true });
+                  } }
+                >
+                  <img src={ shareIcon } alt="ícone de compartilhar" />
+                </button>
+
+                <button
+                  type="button"
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  src={ blackHeartIcon }
+                  onClick={ () => { this.desfavoritar(id); } }
+                >
+                  <img src={ blackHeartIcon } alt="ícone de desfavoritar" />
+                </button>
+
+              </div>
+            );
+          }
           return (
             <div key={ id }>
-              <img
-                src={ image }
-                alt={ name }
-                data-testid={ `${index}-horizontal-image` }
-              />
+              <Link to={ `/drinks/${id}` }>
+                <img
+                  src={ image }
+                  alt={ name }
+                  data-testid={ `${index}-horizontal-image` }
+                  style={ { width: '360px', weight: '640px' } }
+                />
+              </Link>
 
-              <p data-testid={ `${index}-horizontal-top-text` }>{category}</p>
+              <Link to={ `/drinks/${id}` }>
+                <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+              </Link>
 
-              <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+              <p data-testid={ `${index}-horizontal-top-text` }>{alcoholicOrNot}</p>
 
-              <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                onClick={ () => {
+                  clipboardCopy(`http://localhost:3000/drinks/${id}`);
+                  this.setState({ clickShare: true });
+                } }
+              >
                 <img src={ shareIcon } alt="ícone de compartilhar" />
               </button>
 
-              <button type="button" data-testid={ `${index}-horizontal-favorite-btn` }>
-                <img src={ whiteHeartIcon } alt="ícone de favoritar" />
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+                src={ blackHeartIcon }
+                onClick={ () => { this.desfavoritar(id); } }
+              >
+                <img src={ blackHeartIcon } alt="ícone de favoritar" />
               </button>
 
             </div>
@@ -79,6 +177,7 @@ class FavoriteRecipes extends Component {
 
 FavoriteRecipes.propTypes = {
   history: PropTypes.shape({
+    push: PropTypes.func,
     location: PropTypes.shape({
       pathname: PropTypes.string,
     }).isRequired,
